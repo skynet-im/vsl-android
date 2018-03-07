d8779246b6c6f1a158b55a3bbadfda98b98c09a1
Twometer <twometer@outlook.de>
2018-03-07 19:46:41 +0100

    Added Nullable Type

---
+++ app/src/main/java/de/vectordata/jvsl/util/Nullable.java
@@ -0,0 +1,27 @@
+package de.vectordata.jvsl.util;
+
+/**
+ * Created by Twometer on 07.03.2018.
+ * (c) 2018 Twometer
+ */
+
+public class Nullable<T> {
+
+    private T value;
+
+    public Nullable(T value) {
+        this.value = value;
+    }
+
+    public boolean hasValue() {
+        return value != null;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
