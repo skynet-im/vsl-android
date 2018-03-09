a0bc05655ae4ea041c22fe1bfc3345ef5d787f78
Twometer <twometer@outlook.de>
2018-03-09 12:49:32 +0100

    Added example code

---
+++ app/src/main/java/de/vectordata/jvsl/example/EventsExample.java
@@ -0,0 +1,26 @@
+package de.vectordata.jvsl.example;
+
+/**
+ * Created by Twometer on 09.03.2018.
+ * (c) 2018 Twometer
+ */
+
+public class EventsExample {
+
+    private TestListener listener;
+
+    public void setListener(TestListener listener) {
+        this.listener = listener;
+    }
+
+    public void someMethod() {
+        // Some code
+        if(listener != null) listener.onEvent(); // Raise the event
+        // Some more code
+    }
+
+    interface TestListener {
+        void onEvent();
+    }
+
+}
